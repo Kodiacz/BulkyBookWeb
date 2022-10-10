4f67b02723fc660b9eef1ded9f382aac6787c52c
Simeon Yordanov <lookmanche@gmail.com>
2022-10-10 23:55:42 +0300

    migration
    created ApplicationDbContext and created migration with SQL server called initial-migration

---
+++ Bulky-Book/BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
