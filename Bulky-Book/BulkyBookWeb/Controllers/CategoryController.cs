211bc070e55d8bc5922fcb743ccf8d846a69f950
Simeon Yordanov <lookmanche@gmail.com>
2022-10-15 17:08:54 +0300

    created new action Edit in the CategoryController

--- Bulky-Book/BulkyBookWeb/Controllers/CategoryController.cs
+++ Bulky-Book/BulkyBookWeb/Controllers/CategoryController.cs
@@ -43,5 +43,44 @@ namespace BulkyBookWeb.Controllers
 
             return View(obj);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _dbContext.Categories.Find(id);
+            //var categoryFromDbFirst = _dbContext.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _dbContext.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View(obj);
+        }
     }
 }
